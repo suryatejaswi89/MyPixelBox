9fbe229204cb26393013ac94a0c76906e30059d0
Surya Tejaswi <Teja@Suryas-MBP.fios-router.home>
2017-03-14 14:22:24 -0400

    integrated google sign-in

---
+++ config/auth.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '1558643184165087',//ClientID
+        'clientSecret'  : '48dc5942a06f9abc720cb4dc4d4fde66', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    },
+    'googleAuth' : {
+        'clientID'      : '983802961271-u2smg6mv097cn49mbv6a7jqs77esgmfo.apps.googleusercontent.com',
+        'clientSecret'  : '47m7UMJunv6QbObtpgTWqJzS',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    }
+
+  }
